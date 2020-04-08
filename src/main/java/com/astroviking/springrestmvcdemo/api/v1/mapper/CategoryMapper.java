599681037b1b1ae242be4885ffc8987433f04345
Tyler Gray <tyler.gray@gmail.com>
2020-04-07 23:14:12 -0500

    Fixed build.  Added Category controller.

--- src/main/java/com/astroviking/springrestmvcdemo/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/astroviking/springrestmvcdemo/api/v1/mapper/CategoryMapper.java
@@ -4,10 +4,8 @@ import com.astroviking.springrestmvcdemo.api.v1.model.CategoryDTO;
 import com.astroviking.springrestmvcdemo.domain.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Component;
 
 @Mapper
-@Component
 public interface CategoryMapper {
 
   CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
