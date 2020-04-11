d0b8da4e99f4b3b4f5f8bb622879ceb86bfc2952
Tyler Gray <tyler.gray@gmail.com>
2020-04-11 09:58:09 -0500

    Swagger configuration.

--- src/main/java/com/astroviking/springrestmvcdemo/api/v1/model/VendorDTO.java
+++ src/main/java/com/astroviking/springrestmvcdemo/api/v1/model/VendorDTO.java
@@ -1,5 +1,7 @@
 package com.astroviking.springrestmvcdemo.api.v1.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("Vendor")
 public class VendorDTO {
+  @ApiModelProperty("Vendor ID")
   private Long id;
+
+  @ApiModelProperty("Vendor name")
   private String name;
 }
