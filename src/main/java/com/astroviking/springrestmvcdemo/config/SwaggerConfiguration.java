063f78a013d1c43a43a96dad037948b6988da3d4
Tyler Gray <tyler.gray@gmail.com>
2020-04-11 09:30:22 -0500

    Swagger configuration.

---
+++ src/main/java/com/astroviking/springrestmvcdemo/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package com.astroviking.springrestmvcdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .pathMapping("/")
+        .apiInfo(metaData());
+  }
+
+  private ApiInfo metaData() {
+    Contact contact = new Contact("Tyler Gray", "https://astroviking.com", "tyler.gray@gmail.com");
+
+    return new ApiInfo(
+        "Spring REST MVC Demo",
+        "A demo application for Spring REST MVC",
+        "1.0",
+        "Terms fo Service: demo",
+        contact,
+        "Apache License Version 2.0",
+        "https://www.apache.org/licenses/LICENSE-2.0",
+        Collections.emptyList());
+  }
+}
